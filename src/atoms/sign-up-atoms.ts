fe7293da258282f1c825d8fbe470b951189af76e
nicolascasmuz <nicolascasmuz@hotmail.com>
2024-09-19 23:38:19 -0300

    commit-004

--- src/atoms/sign-up-atoms.ts
+++ src/atoms/sign-up-atoms.ts
@@ -1,6 +1,5 @@
 import { atom, selector, useSetRecoilState } from "recoil";
 const API_BASE_URL = "http://localhost:3000";
-import { dataAtom } from "./data-atom";
 
 const signinAtom = atom({
   key: "signin-atom",
@@ -13,27 +12,27 @@ const signinAtom = atom({
 const signinSelector = selector({
   key: "signin-selector",
   get: async ({ get }) => {
-    const emailValue = get(signinAtom)["email"];
-    const passValue = get(signinAtom)["password"];
-    console.log("signinSelector: ", { emailValue, passValue });
-    if (emailValue && passValue) {
+    const emailPassValues = get(signinAtom);
+    console.log("signinSelector: ", {
+      email: emailPassValues.email,
+      password: emailPassValues.password,
+    });
+    if (emailPassValues.email && emailPassValues.password) {
       const response = await fetch(API_BASE_URL + "/sign-up", {
         method: "post",
         headers: {
           "content-type": "application/json",
         },
-        body: JSON.stringify({ emailValue, passValue }),
+        body: JSON.stringify({
+          email: emailPassValues.email,
+          password: emailPassValues.password,
+        }),
       });
       const json = await response.json();
       const data = json.profile;
-      if (data.createdUser == true) {
-        data.newUser = true;
-      } else if (data.createdUser == false) {
-        data.newUser = false;
-      }
       return data;
     } else {
-      return false;
+      return "usuario existente";
     }
   },
 });
