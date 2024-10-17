3f8c91855a143cf131de8a062791e2c3791483f1
nicolascasmuz <nicolascasmuz@hotmail.com>
2024-10-16 22:55:01 -0300

    commit-027

--- backend/lib/algolia.ts
+++ backend/lib/algolia.ts
@@ -1,8 +1,8 @@
 import algoliasearch from "algoliasearch";
 
 const client = algoliasearch(
-  process.env.ALGOLIA_APPID,
-  process.env.ALGOLIA_ADMINAPIKEY
+  process.env.REACT_APP_ALGOLIA_APPID,
+  process.env.REACT_APP_ALGOLIA_ADMINAPIKEY
 );
 
 const missingPetsIndex = client.initIndex("missingpets");
